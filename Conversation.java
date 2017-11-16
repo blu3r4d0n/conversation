50e1a34fb865fdf53caffcab31b6927f6b7a9e39
Nelson <nelsonp8@abernathy.winthrop.edu>
2017-11-16 03:50:10 -0500

    Just fixed up the new code we did today in class. It's in the other branch too.

--- Conversation.java
+++ Conversation.java
@@ -16,7 +16,9 @@ public class Conversation extends JPanel
 	private Timer conversationTimer;
 	private Random gen;
 	private int speakerIndex, listenerIndex;
-	private JButton reportButton;
+	private JButton reportButton, startButton, stopButton;
+	private JPanel rulerPanel, buttonPanel;
+
 	
    //-----------------------------------------------------------------
    //  Initializes various rulers, gets them talking to each other.
@@ -28,22 +30,38 @@ public class Conversation extends JPanel
 		gen = new Random();
 		setBackground(Color.yellow);
 		conversationTimer = new Timer(1000, new ConversationTimerListener());
-		reportButton = new JButton("Report");
-		add(reportButton);
-		reportButton.addActionListener(new ReportButtonListener());
-		setPreferredSize(new Dimension(1100, 900));
-		setLayout(new GridLayout(3,2));
-		createRulers();
+		initButtonPanel();
+		initRulerPanel();
 		identifyRulers();
-		System.out.println();
-		conversationTimer.start();
-		System.out.println();
-		addRulers();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
+	}
+	public void initRulerPanel()
+	{   
+		rulerPanel = new JPanel();
+		add(rulerPanel);
+		rulerPanel.setLayout(new GridLayout(3,2));
+		createRulers();
+		addRulers();
+	}
+	
+	public void initButtonPanel()
+	{
+		buttonPanel = new JPanel();
+		add(buttonPanel);
+		reportButton = new JButton("Report");
+		buttonPanel.add(reportButton);
+		reportButton.addActionListener(new ReportButtonListener());
+		startButton = new JButton("Start");
+		buttonPanel.add(startButton);
+		startButton.addActionListener(new StartButtonListener());
+		stopButton = new JButton("Stop");
+		buttonPanel.add(stopButton);
+		stopButton.addActionListener(new StopButtonListener());
 	}
 	public void createRulers() //adds each ruler to the array and creates rulers
 	{
-	 rulerList[0] = new Ruler( "Abraham Lincoln", "President", "America", "Americans", "Howdy", Color.blue,new ImageIcon("src/images/lincoln.jpeg") );
+	 rulerList[0] = new Ruler( "Abraham Lincoln", "President", "America", "Americans", "Howdy", Color.blue,new ImageIcon("src/images/incoln.jpeg"));
 	 rulerList[1] = new Ruler("Cleopatra", "Queen", "Egypt", "Egyptians", "Marhaban", Color.green, new ImageIcon("src/images/cleo.jpeg"));
 	 rulerList[2] = new Ruler("Hitler", "Fuhrer", "Germany", "Germans", "Guten Tag", Color.red, new ImageIcon("src/images/hitler.jpg"));
 	 rulerList[3] = new Ruler("Napoleon", "Emperor", "France", "French", "Bonjour", Color.pink, new ImageIcon("src/images/napoleon.jpg"));
@@ -77,15 +95,14 @@ public class Conversation extends JPanel
 	{
 	for (int i=0; i < numRulers; i++)
 		{
-		add(rulerList[i]);
+		rulerPanel.add(rulerList[i]);
 		
 	}
 	}
 private class ConversationTimerListener implements ActionListener
 	// Has one ruler say hello when timer expires
 {
-	public void actionPerformed(ActionEvent evt) //ActionListener leaves this blank. We must fill
-		//it in with what we want to do when the timer goes off.
+	public void actionPerformed(ActionEvent evt) //calles the coverse method  every 1000 milliseconds
 	{
 		converse();
 	}
@@ -93,10 +110,25 @@ private class ConversationTimerListener implements ActionListener
 private class ReportButtonListener implements ActionListener
 	// has the rulers report when the button is clicked
 {
-	public void actionPerformed(ActionEvent evt) //ActionListener leaves this blank. We must fill
-		//it in with what we want to do when the timer goes off.
+	public void actionPerformed(ActionEvent evt) //when the report button is clicked, it calls the report method
 	{
 		report();
+	}
+}
+private class StartButtonListener implements ActionListener
+	// This starts the conversation when the button is pushed
+{
+	public void actionPerformed(ActionEvent evt) //starts the conversation timer
+	{
+		conversationTimer.start();
+	}
+}
+private class StopButtonListener implements ActionListener
+	// This stops the conversation when the button is pushed
+{
+	public void actionPerformed(ActionEvent evt) //stops the conversation timer
+	{
+		conversationTimer.stop();
 	}
 }
 }
